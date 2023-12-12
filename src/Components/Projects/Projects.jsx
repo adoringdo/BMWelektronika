7d17cc6bf6e0766b49bbbfa939e7649203b92d66
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-12-12 23:58:08 +0200

    style: add responsive and mobile design

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
       <h1 className={styles.title}>Projects</h1>
       <Container className={styles.container}>
         <Row>
-          <Col className={styles.description}>
+          <Col md={6} className={styles.description}>
             <h1>Project 1</h1>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque,
@@ -20,15 +20,15 @@ export default function Projects() {
             </p>
             <button className={styles.button}>See More</button>
           </Col>
-          <Col className={styles.imageContainer}>
+          <Col md={6} className={styles.imageContainer}>
             <img src={projectmain1} alt="project showcase" />
           </Col>
         </Row>
         <Row>
-          <Col className={styles.imageContainer}>
+          <Col md={6} className={styles.imageContainer}>
             <img src={projectmain2} alt="project showcase" />
           </Col>
-          <Col className={styles.description}>
+          <Col md={6} className={styles.description}>
             <h1>Project 2</h1>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque,
