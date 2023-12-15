0cad94678d08e4f249250d2923c20fcba15c73e2
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-12-15 16:18:32 +0200

    fix: update broken links and icon

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,34 +8,35 @@ import { Element } from 'react-scroll';
 export default function Contact() {
   return (
     <Element id="contact" className={styles.contact}>
-    
-      
-    <h1 className={styles.contact}>Domina? Parašykite mums:</h1>
-        <h1 className={styles.email}>
-          <img src={email} alt="email icon" /> bmwelektronikalt@gmail.com
-        </h1>
-        <span className={styles.text}>A &nbsp; &nbsp; R &nbsp; & nbsp; B &nbsp; & nbsp; A</span>
-        <p className={styles.text}>Susisiekite su mumis:</p>
-        <div className={styles.socials}>
-          <div className={styles.social}>
-            <a href="facebook.com/BMWengineTemperatureMod" target="blank">
-              <img src={facebook} alt="facebook icon" />
-              <div>Facebook</div>
-            </a>
-          </div>
-          <div className={styles.social}>
-            <a href="https://www.ebay.com/itm/285156702744?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=QZxotanyS9u&sssrc=2047675&ssuid=&widget_ver=artemis&media=COPY" target="blank">
-              <img src={ebay} alt="ebay icon" />
-              <div>Ebay</div>
-            </a>
-          </div>
-          <div className={styles.social}>
-            <a href="m.me/104530015573802" target="blank">
-              <img src={messenger} alt="messenger icon" />
-              <div>Messenger</div>
-            </a>
-          </div>
+      <h1 className={styles.contact}>Domina? Parašykite mums:</h1>
+      <h1 className={styles.email}>
+        <img src={email} alt="email icon" /> bmwelektronikalt@gmail.com
+      </h1>
+      <span className={styles.text}>a r b a</span>
+      <p className={styles.text}>Susisiekite su mumis:</p>
+      <div className={styles.socials}>
+        <div className={styles.social}>
+          <a href="https://facebook.com/BMWengineTemperatureMod" target="blank">
+            <img src={facebook} alt="facebook icon" />
+            <div>Facebook</div>
+          </a>
         </div>
+        <div className={styles.social}>
+          <a
+            href="https://www.ebay.com/itm/285156702744?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=QZxotanyS9u&sssrc=2047675&ssuid=&widget_ver=artemis&media=COPY"
+            target="blank"
+          >
+            <img src={ebay} alt="ebay icon" />
+            <div>Ebay</div>
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a href="https://m.me/104530015573802" target="blank">
+            <img src={messenger} alt="messenger icon" />
+            <div>Messenger</div>
+          </a>
+        </div>
+      </div>
       <a
         style={{ display: 'inline-block', marginTop: '3rem', color: '#fff' }}
         href="https://www.flaticon.com/free-icons/email"
