14cf23971789d3c4289304ca386a6d54d3ba973b
adoringdo <adenisovaite@gmail.com>
2023-12-15 14:49:27 +0200

    add social links

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,24 +8,30 @@ export default function Contact() {
   return (
     <section className={styles.contact}>
       <div className={styles.contactBody}>
-        <h1 className={styles.contact}>Interested? Email me at:</h1>
+        <h1 className={styles.contact}>Domina? Parašykite mums:</h1>
         <h1 className={styles.email}>
           <img src={email} alt="email icon" /> bmwelektronikalt@gmail.com
         </h1>
-        <span className={styles.text}>O&nbsp; &nbsp;R</span>
-        <p className={styles.text}>Reach me through my social:</p>
+        <span className={styles.text}>A &nbsp; &nbsp; R &nbsp; & nbsp; B &nbsp; & nbsp; A</span>
+        <p className={styles.text}>Susisiekite su mumis:</p>
         <div className={styles.socials}>
           <div className={styles.social}>
-            <img src={facebook} alt="facebook icon" />
-            <div>Facebook</div>
+            <a href="facebook.com/BMWengineTemperatureMod" target="blank">
+              <img src={facebook} alt="facebook icon" />
+              <div>Facebook</div>
+            </a>
           </div>
           <div className={styles.social}>
-            <img src={ebay} alt="ebay icon" />
-            <div>Ebay</div>
+            <a href="https://www.ebay.com/itm/285156702744?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=QZxotanyS9u&sssrc=2047675&ssuid=&widget_ver=artemis&media=COPY" target="blank">
+              <img src={ebay} alt="ebay icon" />
+              <div>Ebay</div>
+            </a>
           </div>
           <div className={styles.social}>
-            <img src={messenger} alt="messenger icon" />
-            <div>Messenger</div>
+            <a href="m.me/104530015573802" target="blank">
+              <img src={messenger} alt="messenger icon" />
+              <div>Messenger</div>
+            </a>
           </div>
         </div>
       </div>
