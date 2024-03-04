a6fa483b0b0e8a6686ce0ac950f2a5f10f11a808
adoringdo <adenisovaite@gmail.com>
2024-03-04 22:40:11 +0200

    replace project information

--- src/Components/Projects/data.js
+++ src/Components/Projects/data.js
@@ -2,7 +2,6 @@ import projectmain1 from '../../assets/projectmain1.png';
 import projectmain2 from '../../assets/projectmain2.png';
 import firstProjectImg1 from '../../assets/project1/project image.jpg';
 import firstProjectImg2 from '../../assets/project1/project image 2.jpg';
-import firstProjectImg3 from '../../assets/project1/project image 3.jpg';
 import firstInstallImg3 from '../../assets/project1/install.jpg';
 import secondProjectImg1 from '../../assets/project2/project image.jpg';
 import secondProjectImg2 from '../../assets/project2/project image 2.jpg';
@@ -10,19 +9,19 @@ import secondProjectImg2 from '../../assets/project2/project image 2.jpg';
 export const projects = [
   {
     mainImg: projectmain1,
-    title: 'Projektas 1',
+    title: 'Modulis 1',
     description:
       'Variklio temperatūros vietoj autopiloto modas E60, E70, E90 automobiliams',
-    pictures: [firstProjectImg3, firstProjectImg2, firstProjectImg1],
+    pictures: [firstProjectImg2, firstProjectImg1],
     install: firstInstallImg3,
     installDescription:
       'LCI modeliams reikalinga nedidelė spidometro modifikacija',
   },
   {
     mainImg: projectmain2,
-    title: 'Projektas 2',
+    title: 'Modulis 2',
     description:
       'M3 stiliaus variklio temperatūra E90 serijos dyzeliniams automobiliams',
-    pictures: [secondProjectImg1, secondProjectImg2],
+    pictures: [],
   },
 ];
