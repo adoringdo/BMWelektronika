9a3f0407ff2a7a4cefaa955ba196614fa1a8a586
Khaled <aa.khaled11@gmail.com>
2024-12-28 14:58:28 +0200

    update contacts information section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,9 @@ import styles from "./Contact.module.css";
 import email from "../../assets/email.png";
 import facebook from "../../assets/facebook.png";
 import messenger from "../../assets/messenger.png";
-import ebay from "../../assets/ebay.png";
+import instagram from "../../assets/instagram.png";
+import paypal from "../../assets/paypal.png";
+import youtube from "../../assets/youtube.png";
 import { Element } from "react-scroll";
 import { useContext } from "react";
 import { LanguageContext } from "../Contexts/languageContext";
@@ -22,18 +24,9 @@ export default function Contact() {
       <p className={styles.text}>{translate(language, 'contacts.contact us')}</p>
       <div className={styles.socials}>
         <div className={styles.social}>
-          <a href="https://facebook.com/BMVelektronika" target="blank">
+          <a href="https://www.facebook.com/BMVelektronika" target="blank">
             <img src={facebook} alt="facebook icon" />
             <div>Facebook</div>
-          </a>
-        </div>
-        <div className={styles.social}>
-          <a
-            href="https://www.ebay.com/itm/285156702744?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=QZxotanyS9u&sssrc=2047675&ssuid=&widget_ver=artemis&media=COPY"
-            target="blank"
-          >
-            <img src={ebay} alt="ebay icon" />
-            <div>Ebay</div>
           </a>
         </div>
         <div className={styles.social}>
@@ -42,9 +35,27 @@ export default function Contact() {
             <div>Messenger</div>
           </a>
         </div>
+        <div className={styles.social}>
+          <a href="https://instagram.com/bmvelektronika" target="blank">
+            <img src={instagram} alt="instagram icon" />
+            <div>Instagram</div>
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a href="https://www.youtube.com/@bmvelektronika/videos" target="blank">
+            <img src={youtube} alt="youtube icon" />
+            <div>Youtube</div>
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a href="https://paypal.me/BMVelektronika" target="blank">
+            <img src={paypal} alt="paypal icon" />
+            <div>Paypal</div>
+          </a>
+        </div>
       </div>
       <a
-        style={{ display: "inline-block", marginTop: "3rem", color: "#fff" }}
+        style={{ display: "inline-block", marginTop: "3rem", color: "#fff", fontSize: ".6rem" }}
         href="https://www.flaticon.com/free-icons/email"
         title="email icons"
         target="blank"
